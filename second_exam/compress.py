3a9284ed6d89d4ad3bf81d9c0ae975217f5323b7
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-15 16:48:18 -0500

    Add compress utility

---
+++ second_exam/compress.py
@@ -0,0 +1,13 @@
+import sys
+
+from huffman.compression import HuffmanEncoder
+
+algortihm = sys.argv[1]
+input_file_name = sys.argv[2]
+output_file_name = sys.argv[3]
+
+if algortihm == "huffman" or algortihm == "h":
+    huffmanEncoder = HuffmanEncoder(input_file_name, output_file_name)
+    huffmanEncoder.encode()
+else:
+    print("The algorithm is not currently supported")
