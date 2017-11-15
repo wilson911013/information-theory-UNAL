ca2e226e78e8a3051f21eebd986148507bcfdc0c
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-15 17:26:55 -0500

    Decompress utility

---
+++ second_exam/decompress.py
@@ -0,0 +1,13 @@
+import sys
+
+from huffman.compression import HuffmanDecoder
+
+algortihm = sys.argv[1]
+input_file_name = sys.argv[2]
+output_file_name = sys.argv[3]
+
+if algortihm == "huffman" or algortihm == "h":
+    huffmanDecoder = HuffmanDecoder(input_file_name, output_file_name)
+    huffmanDecoder.decode()
+else:
+    print("The algorithm is not currently supported")
