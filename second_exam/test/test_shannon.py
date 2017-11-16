b31f21ba620fb06d1ecc8ab77aa1fc29cee800bf
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-16 13:40:45 -0500

    Everithing working

--- second_exam/test/test_shannon.py
+++ second_exam/test/test_shannon.py
@@ -19,17 +19,17 @@ class TestShannon(unittest.TestCase):
         decompressed_file = "test_files/goo_decompressed.txt"
         self.compress_and_decompress_util(original_file, decompressed_file)
 
-    # def test_encoding_text_files(self):
-    #     original_file = "test_files/text.txt"
-    #     decompressed_file = "test_files/text_decompressed.txt"
-    #     self.compress_and_decompress_util(original_file, decompressed_file)
+    def test_encoding_text_files(self):
+        original_file = "test_files/text.txt"
+        decompressed_file = "test_files/text_decompressed.txt"
+        self.compress_and_decompress_util(original_file, decompressed_file)
 
-    # def test_encoding_wav_files(self):
-    #     original_file = "test_files/c.wav"
-    #     decompressed_file = "test_files/c_decompressed.wav"
-    #     self.compress_and_decompress_util(original_file, decompressed_file)
+    def test_encoding_wav_files(self):
+        original_file = "test_files/c.wav"
+        decompressed_file = "test_files/c_decompressed.wav"
+        self.compress_and_decompress_util(original_file, decompressed_file)
 
-    # def test_encoding_long_wav_files(self):
-    #     original_file = "test_files/wind_of_change.wav"
-    #     decompressed_file = "test_files/wind_of_change_decompressed.wav"
-    #     self.compress_and_decompress_util(original_file, decompressed_file)
+    def test_encoding_long_wav_files(self):
+        original_file = "test_files/wind_of_change.wav"
+        decompressed_file = "test_files/wind_of_change_decompressed.wav"
+        self.compress_and_decompress_util(original_file, decompressed_file)
