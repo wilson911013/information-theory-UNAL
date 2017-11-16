c57a233490866122513c1c1d40cf312fb6981317
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-11-16 13:21:01 -0500

    Shannon with compress and decompress utility

--- second_exam/compress.py
+++ second_exam/compress.py
@@ -1,6 +1,7 @@
 import sys
 
 from compression.huffman import HuffmanEncoder
+from compression.shannon import ShannonFanoEncoder
 
 algortihm = sys.argv[1]
 input_file_name = sys.argv[2]
@@ -9,5 +10,8 @@ output_file_name = sys.argv[3]
 if algortihm == "huffman" or algortihm == "h":
     huffmanEncoder = HuffmanEncoder(input_file_name, output_file_name)
     huffmanEncoder.encode()
+elif algortihm == "shannon" or algortihm == "s":
+    shannonFanoEncoder = ShannonFanoEncoder(input_file_name, output_file_name)
+    shannonFanoEncoder.encode()
 else:
     print("The algorithm is not currently supported")
--- second_exam/decompress.py
+++ second_exam/decompress.py
@@ -1,6 +1,7 @@
 import sys
 
 from compression.huffman import HuffmanDecoder
+from compression.shannon import ShannonFanoDecoder
 
 algortihm = sys.argv[1]
 input_file_name = sys.argv[2]
@@ -9,5 +10,8 @@ output_file_name = sys.argv[3]
 if algortihm == "huffman" or algortihm == "h":
     huffmanDecoder = HuffmanDecoder(input_file_name, output_file_name)
     huffmanDecoder.decode()
+elif algortihm == "shannon" or algortihm == "s":
+    shannonFanoDecoder = ShannonFanoDecoder(input_file_name, output_file_name)
+    shannonFanoDecoder.decode()
 else:
     print("The algorithm is not currently supported")
